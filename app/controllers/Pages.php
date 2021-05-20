3d66866ce2561a05b95b3d75c13d0351fbe2bca7
arianrah <arianrahimi@icloud.com>
2021-05-20 13:10:14 -0700

    Config setup, EZPHP init.

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -8,7 +8,7 @@ class Pages extends Controller
   public function index()
   {
     $data = [
-      'title' => 'Welcome',
+      'title' => 'SimplyBlog',
     ];
     $this->view('pages/index', $data);
   }
--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,4 +1,3 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <h1><?php echo $data['title']; ?></h1>
-<o>This is the EZPHP framework. Config and setup found on ~/README.md</p>
-  <?php require APPROOT . '/views/inc/footer.php'; ?>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
